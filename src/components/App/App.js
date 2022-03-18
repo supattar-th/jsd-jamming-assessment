a434d96596686e8cfebd12f69e0c5d15d4a390d2
supattar thueanthanon <supattar3522@gmail.com>
2022-03-18 14:48:42 +0700

    waiting for update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,62 @@
+import React from 'react';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 
-function App() {
-  return (
-    <div>
-      Put components here to make them show up in the browser. Good Luck!
-    </div>
-  );
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1}, {name: 'name2', artist: 'artist2', album: 'album2', id: 2}, {name: 'name3', artist: 'artist3', album: 'album3', id: 3}],
+      playListName: 'My Playlist',
+      playlistTracks: [{name: 'platlistName1', artist: 'playlistArtist1', album: 'playlistAlbum1', id: 4}, {name: 'platlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2', id: 5}, {name: 'platlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6}]
+    };
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === Track.id)) {
+      return;
+    }
+  
+  tracks.push(track);
+  this.setState({ playlistTracks: tracks });
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = track.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState({ playlistTracks: tracks });
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} 
+            onAdd={this.addTrack} /> 
+            <Playlist playlistName={this.state.playlistName}
+            playlistTracks={this.state.playlistTracks}
+            onRemove={this.removeTrack} />
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
+      
+   
 
 export default App;
--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,7 +1,20 @@
+import React from 'react';
 import './Playlist.css';
 
-function Playlist() {
-  return <div></div>;
+import TrackList from '../TrackList/TrackList';
+
+class Playlist extends React.Component {
+  render () {
+    return (
+      <div class="Playlist">
+        <input defaultValue="New Playlist"/>
+        <TrackList tracks={this.props.playlistTracks} 
+          onRemove={this.props.onRemove}
+          isRemoval={true} />
+        <button class="Playlist-save">SAVE TO SPOTIFY</button>
+      </div>
+    )
+  }
 }
 
 export default Playlist;
